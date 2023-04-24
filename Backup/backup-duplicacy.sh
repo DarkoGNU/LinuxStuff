1e677dc781acbff78dd5d418eccc4b3fb0fe0058
DarkoGNU <42816979+DarkoGNU@users.noreply.github.com>
2023-04-24 13:28:38 +0200

    Duplicacy Backup basically done

--- Backup/backup-duplicacy.sh
+++ Backup/backup-duplicacy.sh
@@ -8,27 +8,30 @@
 
 # Functions
 info () { echo -e "\e[32m[INFO]\e[0m ${1}" ; }
+warn () { echo -e "\e[33m[WARN]\e[0m ${1}" ; }
 error () { echo -e "\e[31m[INFO]\e[0m ${1}" ; }
 
 # Duplicacy config
 BASE_FOLDER="/opt/duplicacy/data"         # Base folder for all data used by script
 REPO_FOLDER="${BASE_FOLDER}/repositories" # Folder for storing repository data (what normally is stored in .duplicacy)
 MOUNT_FOLDER="${BASE_FOLDER}/mounts"      # Folder where snapshots will be mounted
+THREADS=4                                 # Number of threads to use
 
-STORAGE_NAME="google-drive" # Name of the storage used
-STORAGE="gcd://duplicacy"   # URL of the storage used
+STORAGE_NAME="google-drive"               # Name of the storage used
+STORAGE="gcd://duplicacy"                 # URL of the storage used
+GCD_TOKEN="${BASE_FOLDER}/gcd-token.json" # Path to Google Drive token
 
 # Repositories and locations
-REPOSITORIES=("ampere-home") # "ampere-etc" "ampere-root") # Names of the repositories
-LOCATIONS=("home/" "etc/" "root/")                            # Folder inside the volume to back up (empty to back up root of the volume)
+REPOSITORIES=("ampere-etc" "ampere-root" "ampere-home") # Names of the repositories
+LOCATIONS=("etc/" "root/" "home/")                      # Folder inside the volume to back up (empty to back up root of the volume)
 
 # Drive configuration
 SNAP_PREFIX=duplicacy-
 VOLUMES=("ocivolume/root" "ocivolume/root" "ocivolume/root")
 
 info "Ensuring that base directories are created"
-echo mkdir -p "$REPO_FOLDER"
-echo mkdir -p "$MOUNT_FOLDER"
+mkdir -p "$REPO_FOLDER"
+mkdir -p "$MOUNT_FOLDER"
 
 # Backup all repositories
 for (( i=0; i<${#REPOSITORIES[@]}; i++ )); do
@@ -47,23 +50,29 @@ for (( i=0; i<${#REPOSITORIES[@]}; i++ )); do
 	info "Backing up repository ${REPO}"
 
 	info "Creating a snapshot"
-	echo lvcreate --snapshot --name "$SNAPSHOT" "$VOLUME"
+	lvcreate --snapshot --name "$SNAPSHOT" "$VOLUME"
 
 	info "Mounting the snapshot"
-	echo mkdir -p "$MOUNT"
-	echo lvchange -ay -Ky "$SNAPSHOT"
-	echo mount -o nouuid,ro "/dev/${SNAPSHOT}" "$MOUNT"
+	mkdir -p "$MOUNT"
+	lvchange -ay -Ky "$SNAPSHOT"
+	mount -o nouuid,ro "/dev/${SNAPSHOT}" "$MOUNT"
 
 	info "Checking if the repo is initialized"
         if [[ ! -d "$REPO_PATH" ]]; then
 		info "Initializing the repository"
-                echo duplicacy init -pref-dir "$REPO_PATH" -repository "$BACKUP_PATH" -storage-name "$STORAGE_NAME" "$REPO" "$STORAGE"
+		warn "Please wait, the token path will be passed automatically!"
+		(sleep 5; echo "$GCD_TOKEN") | duplicacy init -pref-dir "$REPO_PATH" -repository "$BACKUP_PATH" -storage-name "$STORAGE_NAME" "$REPO" "$STORAGE"
         fi
 
+	info "Starting the actual backup"
+	echo -n "$REPO_PATH" > .duplicacy
+	warn "Please wait, the token path will be passed automatically!"
+	(sleep 5; echo "$GCD_TOKEN") | duplicacy backup -threads "$THREADS" -stats
+
 	info "Unmounting the snapshot"
-	echo umount "/dev/${SNAPSHOT}"
+	umount "/dev/${SNAPSHOT}"
 	info "Removing the mount point"
-	echo rmdir "$MOUNT"
+	rmdir "$MOUNT"
 	info "Removing the snapshot"
-	echo lvremove -y "/dev/${SNAPSHOT}"
+	lvremove -y "/dev/${SNAPSHOT}"
 done
